42acc3e640312cb0ac807e3423d9915c2d4c79e2
Bill Seremetis <bill@seremetis.net>
2013-12-04 00:12:08 +0200

    now downloads

--- clisubber.sh
+++ clisubber.sh
@@ -1,11 +1,29 @@
 #!/bin/bash
-cwd=$(pwd)
-echo $cwd;
+DEFAULT_LANG="el"
+while getopts ":i" opts;
+do
+  case ${opts} in
+    i) echo "interactive on"
+       INTERACTIVE_VAL="-i" ;;
+    \?) echo "Invalid option: -$OPTARG" >&2 ;;
+  esac
+done
 
-for sub in "$cwd"/*.srt
+if [ -z "$1" ]
+  then SUBLANG_VAL=$DEFAULT_LANG
+  else SUBLANG_VAL=$1
+fi
+
+cwd=$(pwd)
+for vid in "$cwd"/*.mkv "$cwd"/*.mp4 "$cwd"/*.avi;
+do
+  [ -e "$vid" ] || continue
+  subdownloader -c $INTERACTIVE_VAL -l $SUBLANG_VAL --rename-subs --video="$vid"
+done
+
+for sub in "$cwd"/*.srt "$cwd"/*.sub;
 do
   [ -e "$sub" ] || continue
-  echo "$sub"
   subenc=$(enca -L none -i "$sub")
   if [ "$subenc" != "UTF-8" ]
   then
